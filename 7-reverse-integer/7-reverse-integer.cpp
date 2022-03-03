3c6825e5e8b0eca4fc6ace82fe3c11471e5df76d
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-03-03 19:27:00 +0530

    Time: 4 ms (47.01%), Space: 6.2 MB (6.48%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,42 @@
+#include <math.h>
+#include <climits>
+class Solution {
+public:
+    int length(int x)
+{
+    if (x <= 9)
+    {
+        return 1;
+    }
+    return length(x / 10) + 1;
+}
+
+int reversehelper(int x)
+{
+    long long int sum = 0;
+    if(x >= 2147483647 || x <= -2147483648 ){return 0;}
+    int n = length(x);
+    int i = x;
+    int k = 0;
+    while (i > 0)
+    {
+        if(i >= 2147483647 || i<= -2147483648 ){return 0;}
+        int a = i % 10;
+        i = i / 10;
+        sum += (a * pow(10, n - k - 1));
+         if(sum >= 2147483647 || sum <= -2147483648 ){return 0;}
+        k++;
+    }
+    return sum;
+}
+
+int reverse(int x)
+{
+    if(x >= 2147483647 || x <= -2147483648 ){return 0;}
+    if (x < 0)
+    {
+        return -reversehelper(-x);
+    }
+    return reversehelper(x);
+}
+};
