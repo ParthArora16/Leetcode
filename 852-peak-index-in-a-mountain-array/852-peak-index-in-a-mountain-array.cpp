6d7cee3480f0e1536998de3b97617e54009cf0e8
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-09 20:27:07 +0530

    Time: 11 ms (75.77%), Space: 11.5 MB (50.09%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int size = arr.size();
-        int start = 0, end = size - 1, mid, num;
-        
-        while(start <= end)
-        {
-            mid = start + (end - start) / 2;
-            num = arr[mid];
-            
-            // Main body of the binary search.
-            if(num > arr[mid + 1] && num > arr[mid - 1])
-                return mid;
-            else if(num < arr[mid + 1])
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-        return -1;
+    int peakIndexInMountainArray(vector<int> &ar)
+{
+    int size = ar.size();
+    int start = 0, end = size - 1, mid, num;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        num = ar[mid];
+
+        // Main body of the binary search.
+        if (num > ar[mid + 1] && num > ar[mid - 1])
+            return mid;
+        else if (num < ar[mid + 1])
+            start = mid + 1;
+        else
+            end = mid - 1;
     }
+    return -1;
+}
 };
