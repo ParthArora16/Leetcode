00fb6a2f8986a8585eea91028d9a9a7c3271e9be
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-09 21:37:18 +0530

    Time: 46 ms (51.56%), Space: 25.7 MB (99.85%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -4,28 +4,24 @@ public:
 {
     int n = ar.size();
     vector<int> ans;
-    int i = 0;
-    int j = n - 1;
+    ans.resize(n, 0);
+    int i = 0, j = n - 1, ei = n - 1;
     while (i <= j)
     {
         if (abs(ar[i]) > abs(ar[j]))
         {
-            ans.push_back(ar[i] * ar[i]);
+            // ans.push_back(ar[i] * ar[i]);
+            ans[ei] = ar[i] * ar[i];
+            ei--;
             i++;
         }
         else
         {
-            ans.push_back(ar[j] * ar[j]);
+            // ans.push_back(ar[j] * ar[j]);
+            ans[ei] = ar[j] * ar[j];
+            ei--;
             j--;
         }
-    }
-    i = 0;
-    j = n - 1;
-    while (i < j)
-    {
-        swap(ans[i], ans[j]);
-        i++;
-        j--;
     }
     return ans;
 }
