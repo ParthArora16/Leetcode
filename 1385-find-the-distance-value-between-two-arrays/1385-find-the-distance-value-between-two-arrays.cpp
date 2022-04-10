79c56befddb7d7931f0aa843027d6abbdb23a2ed
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-10 16:34:51 +0530

    Time: 50 ms (10.58%), Space: 13 MB (95.54%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int findTheDistanceValue(vector<int> &arr1, vector<int> &arr2, int d)
+    int findclosest(vector<int> &ar, int target)
 {
+    int ans = INT_MAX;
+    int n = ar.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (abs(target - ar[i]) < ans)
+        {
+            ans = abs(target - ar[i]);
+        }
+    }
+    return ans;
+}
+
+// this fun() will return final ans
+int findTheDistanceValue(vector<int> &arr1, vector<int> &arr2, int d)
+{
+    sort(arr2.begin(), arr2.end());
     int n = arr1.size(), m = arr2.size(), count = 0;
     for (int i = 0; i < n; i++)
     {
-        bool flag = false;
-        for (int j = 0; j < m; j++)
-        {
-            if (abs(arr1[i] - arr2[j]) <= d)
-            {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag)
+        int ans = findclosest(arr2, arr1[i]);
+        if (ans > d)
         {
             count++;
         }
     }
-        return count;
+    return count;
 }
 };
