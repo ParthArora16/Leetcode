3665a9eaf00be21b115976ce697bc14824a93da0
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-10 11:54:54 +0530

    Time: 22 ms (31.46%), Space: 13 MB (74.08%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-    int count=0;
-    for(int i=0;i<arr1.size();i++)
+    int findTheDistanceValue(vector<int> &arr1, vector<int> &arr2, int d)
+{
+    int n = arr1.size(), m = arr2.size(), count = 0;
+    for (int i = 0; i < n; i++)
     {
         bool flag = false;
-        for(int j=0;j<arr2.size();j++)
+        for (int j = 0; j < m; j++)
         {
-            if(abs(arr1[i]-arr2[j]) <= d)
+            if (abs(arr1[i] - arr2[j]) <= d)
             {
-                   flag = true;
+                flag = true;
+                break;
             }
         }
-            
-        if(flag == false)
+        if (!flag)
         {
-            count++;  
-        }   
+            count++;
+        }
     }
-    return count;     
+        return count;
 }
 };
