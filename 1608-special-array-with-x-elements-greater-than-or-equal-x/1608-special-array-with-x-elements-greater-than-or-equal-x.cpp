a37a34fdbcb06a61fb0a2cd31c7d6b2dede10a24
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-15 00:01:40 +0530

    Time: 8 ms (15.58%), Space: 8.6 MB (8.26%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int getans(vector<int> &ar, int x)
+{
+    for (int i = 0; i < ar.size(); i++)
+    {
+        if (ar[i] >= x)
+        {
+            return ar.size() - i;
+        }
+    }
+    return 0;
+}
+
+int specialArray(vector<int> &ar)
+{
+    sort(ar.begin(), ar.end());
+    int n = ar.size();
+    unordered_map<int, int> map;
+    for (int i = 0; i < n; i++)
+    {
+        if (map[ar[i]] == 0)
+        {
+            map[ar[i]] = n - i;
+        }
+    }
+    int x = 0;
+    while (x++ <= n)
+    {
+        if (map.count(x) == 1)
+        {
+            if (map[x] == x)
+            {
+                return x;
+            }
+        }
+        else
+        {
+            int a = getans(ar, x);
+            if (a == x)
+            {
+                return x;
+            }
+        }
+    }
+    return -1;
+}
+
+};
