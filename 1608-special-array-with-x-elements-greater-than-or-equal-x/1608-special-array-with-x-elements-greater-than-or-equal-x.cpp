121867fc91b7916a70806e5e15e2eed7e5e9a77e
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-15 00:08:01 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (90.03%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -16,35 +16,14 @@ int specialArray(vector<int> &ar)
 {
     sort(ar.begin(), ar.end());
     int n = ar.size();
-    unordered_map<int, int> map;
-    for (int i = 0; i < n; i++)
+    int x = 0;
+    while (x++ <= n)
     {
-        if (map[ar[i]] == 0)
+        if (getans(ar,x) == x)
         {
-            map[ar[i]] = n - i;
+            return x;
         }
-    }
-    int x = 1;
-    while (x <= n)
-    {
-        if (map.count(x) == 1)
-        {
-            if (map[x] == x)
-            {
-                return x;
-            }
-        }
-        else
-        {
-            int a = getans(ar, x);
-            if (a == x)
-            {
-                return x;
-            }
-        }
-        x++;
     }
     return -1;
 }
-
 };
