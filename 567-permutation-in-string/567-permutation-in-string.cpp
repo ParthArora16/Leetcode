4adf9c9e788c8e7fec6f28979b4df388ef464cd3
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-14 17:28:50 +0530

    Time: 2440 ms (5.03%), Space: 373.6 MB (5.02%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   bool checkInclusion(string s1, string s2, map<char, int> &map1)
+{
+    if (s1.length() == 0)
+    {
+        return true;
+    }
+    if (s1.length() > s2.length())
+    {
+        return false;
+    }
+    map<char, int> mymap;
+    int i = 0;
+    while (i < s1.size())
+    {
+        mymap[s2[i]]++;
+        i++;
+    }
+    if (map1 == mymap)
+    {
+        return true;
+    }
+    return checkInclusion(s1, s2.substr(1), map1);
+}
+
+bool checkInclusion(string s1, string s2)
+{
+    map<char, int> map1;
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
+        map1[s1[i]]++;
+    }
+    return checkInclusion(s1, s2, map1);
+}
+};
