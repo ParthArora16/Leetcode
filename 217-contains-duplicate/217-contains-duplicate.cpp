769cd7145714574bf54310456bb35b26cf80d0a9
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-04-17 08:06:23 +0530

    Time: 114 ms (64.07%), Space: 51.3 MB (62.64%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -5,7 +5,7 @@ public:
     unordered_map<int, int> map;
     for (int i = 0; i < nums.size(); i++)
     {
-        if (map.count(nums[i]) == 1)
+        if (map[nums[i]] >= 1)
         {
             return true;
         }
