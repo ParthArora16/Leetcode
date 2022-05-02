7ab925b513111d9584b7a5d57d0fad96b49fff5c
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-02 21:56:15 +0530

    Time: 353 ms (5.02%), Space: 14 MB (15.80%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,32 +1,72 @@
 class Solution {
 public:
-    bool isMatch(string s, string p)
+    bool isMatch(string s, string p, int m, int n, bool **ar)
 {
-    // base case Empty pattern
     if (p.size() == 0)
     {
         return s.size() == 0;
     }
-    // handling case when my * is at index 2 or 2nd char as * also means 0 char.
-    if (p.length() > 1 && p[1] == '*')
+
+    if (ar[m][n] != false)
     {
-        if (isMatch(s, p.substr(2)))
-        {
-            return true;
-        }
-        if ((s[0] == p[0] || p[0] == '.') && s.length() > 0) // eg - aab , a*b -> ab , a*b; 
-        {
-            return isMatch(s.substr(1), p);
-        }
-        return false;
+        return ar[m][n];
     }
+
+    if (p.size() > 1 && p[1] == '*')
+    {
+        // case 1
+        if (isMatch(s, p.substr(2), m, n - 2, ar))
+        {
+            ar[m][n] = true;
+            return ar[m][n];
+        }
+        if (s.length() > 0 && (s[0] == p[0] || p[0] == '.'))
+        {
+            ar[m][n] = isMatch(s.substr(1), p, m - 1, n, ar);
+            return ar[m][n];
+        }
+        ar[m][n] = false;
+        return ar[m][n];
+    }
+
     else
     {
         if ((s[0] == p[0] || p[0] == '.') && s.size() > 0)
         {
-            return isMatch(s.substr(1),p.substr(1));
+            ar[m][n] = isMatch(s.substr(1), p.substr(1), m - 1, n - 1, ar);
+            return ar[m][n];
         }
-        return false;
+        ar[m][n] = false;
+        return ar[m][n];
     }
+    return ar[m][n];
+}
+
+bool isMatch(string s, string p)
+{
+    int m = s.length() + 1;
+    int n = p.length() + 1;
+
+    bool **ar = new bool *[m];
+
+    for (int i = 0; i < m; i++)
+    {
+        ar[i] = new bool[n];
+        for (int j = 0; j < n; j++)
+        {
+            ar[i][j] = false;
+        }
+    }
+
+    bool ans = isMatch(s, p, m - 1, n - 1, ar);
+
+    for (int i = 0; i < m; i++)
+    {
+        delete[] ar[i];
+    }
+
+    delete[] ar;
+
+    return ans;
 }
 };
