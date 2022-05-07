615d323ec2252d1f1f9f6c97197d20e5572e17e1
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-07 13:03:53 +0530

    Time: 696 ms (5.06%), Space: 28.8 MB (40.07%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,18 +1,52 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int> &nums)
+    void Merge(vector<int> &ar, int si, int ei)
 {
-    priority_queue<int , vector<int> , greater<int>> pq;
-    for (int i = 0; i < nums.size(); i++)
+    int i = si;
+    int mid = (si + ei) / 2;
+    int j = mid + 1;
+    int n = ei - si + 1;
+    int arr[100000];
+    int k = si;
+    while (i <= mid && j <= ei)
     {
-        pq.push(nums[i]);
+        if (ar[i] < ar[j])
+        {
+            arr[k++] = ar[i++];
+        }
+        else
+        {
+            arr[k++] = ar[j++];
+        }
     }
-    int i = 0;
-    while (!pq.empty())
+    while (i <= mid)
     {
-        nums[i++] = pq.top();
-        pq.pop();
+        arr[k++] = ar[i++];
     }
+    while (j <= ei)
+    {
+        arr[k++] = ar[j++];
+    }
+    for (int i = si; i <= ei; i++)
+    {
+        ar[i] = arr[i];
+    }
+}
+
+void MergeSort(vector<int> &ar, int si, int ei)
+{
+    if (si >= ei)
+    {
+        return;
+    }
+    MergeSort(ar, si, (si + ei) / 2);
+    MergeSort(ar, (si + ei) / 2 + 1, ei);
+    Merge(ar, si, ei);
+}
+
+vector<int> sortArray(vector<int> &nums)
+{
+    MergeSort(nums, 0, nums.size() - 1);
     return nums;
 }
 };
