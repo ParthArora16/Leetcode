c40a82f47de82f2f450ebc8442d632cf2fae2fbb
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-09 20:25:57 +0530

    Time: 4 ms (86.34%), Space: 10.1 MB (79.61%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     vector<int> intersection(vector<int> &ar1, vector<int> &ar2)
+{
+    sort(ar2.begin(), ar2.end());
+    unordered_set<int> s1;
+    vector<int> ans;
+    for (int i = 0; i < ar1.size(); i++)
     {
-        unordered_map<int, int> m;
-        for (int i = 0; i < ar2.size(); i++)
+        int lo = 0;
+        int hi = ar2.size() - 1;
+        int target = ar1[i];
+        int mid;
+        bool flag = false;
+        while (lo <= hi)
         {
-            m[ar2[i]]++;
-        }
-        vector<int> ans;
-        for (int i = 0; i < ar1.size(); i++)
-        {
-            if (m.count(ar1[i]) > 0)
+            mid = (lo + hi) / 2;  
+            if (ar2[mid] == target)
             {
-                ans.push_back(ar1[i]);
-                m.erase(ar1[i]);
+                flag = true;
+                break;
+            }
+            else if (target > ar2[mid])
+            {
+                lo = mid + 1;
+            }
+            else
+            {
+                hi = mid - 1;
             }
         }
-        return ans;
+        if (flag == true && s1.count(ar1[i]) == 0)
+        {
+            s1.insert(ar1[i]);
+            ans.push_back(ar1[i]);
+        }
     }
+    return ans;
+}
 };
