6d72dd89cef204965c4897cb3092ef71dfb652ec
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-11 13:16:46 +0530

    Time: 14 ms (13.29%), Space: 16.5 MB (47.32%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -13,13 +13,9 @@ class Solution {
 public:
     bool isSymmetric(TreeNode *root1, TreeNode *root2)
 {
-    if (root1 == NULL && root2 == NULL)
-    {
-        return true;
-    }
     if (root1 == NULL || root2 == NULL)
     {
-        return false;
+        return root1 == root2;
     }
     if (root1->val != root2->val)
     {
@@ -27,8 +23,13 @@ public:
     }
     return isSymmetric(root1->left, root2->right) && isSymmetric(root1->right, root2->left);
 }
+
 bool isSymmetric(TreeNode *root)
 {
+    if (root == NULL)
+    {
+        return true;
+    }
     return isSymmetric(root->left, root->right);
 }
 };
