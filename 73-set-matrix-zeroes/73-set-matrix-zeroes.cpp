a0f39237d5512ab81d3187b3f93543dbbfb41bc4
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-11 12:00:42 +0530

    Time: 23 ms (32.64%), Space: 13.1 MB (82.66%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,28 +2,40 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>> &matrix)
 {
-    vector<pair<int, int>> v;
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix[i].size(); j++)
         {
             if (matrix[i][j] == 0)
             {
-                v.push_back({i, j});
+                matrix[i][j] = INT_MAX - 1;
             }
         }
     }
-    for (int k = 0; k < v.size(); k++)
+    for (int i = 0; i < matrix.size(); i++)
     {
-        int x = v[k].first;
-        int y = v[k].second;
-        for (int i = 0; i < matrix.size(); i++)
+        for (int j = 0; j < matrix[i].size(); j++)
         {
-            matrix[i][y] = 0;
-        }
-        for (int j = 0; j < matrix[x].size(); j++)
-        {
-            matrix[x][j] = 0;
+            if (matrix[i][j] == INT_MAX - 1)
+            {
+                matrix[i][j] = 0;
+                int x = i;
+                int y = j;
+                for (int k = 0; k < matrix[x].size(); k++)
+                {
+                    if (matrix[x][k] != INT_MAX - 1)
+                    {
+                        matrix[x][k] = 0;
+                    }
+                }
+                for (int k = 0; k < matrix.size(); k++)
+                {
+                    if (matrix[k][y] != INT_MAX - 1)
+                    {
+                        matrix[k][y] = 0;
+                    }
+                }
+            }
         }
     }
 }
