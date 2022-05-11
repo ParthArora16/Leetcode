3d567ccf39a366f8d0c09749f5e8182aff60c8de
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-11 11:06:15 +0530

    Time: 183 ms (74.98%), Space: 83 MB (78.82%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int firstMissingPositive(vector<int> &ar)
 {
-    sort(ar.begin(), ar.end());
-    unique(ar.begin(), ar.end());
+    int n = ar.size();
+    // remove all negative element by n + 1 as it can act as a default value because
+    //  my ans set likes b/w 1 ..... n + 1 , and my n + 1 is out of range of array.
 
-    int k = 1;
-    for (int i = 0; i < ar.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        if (ar[i] > 0)
+        if (ar[i] <= 0)
         {
-            if (ar[i] == k)
-            {
-                k++;
-            }
-            else
-            {
-                return k;
-            }
+            ar[i] = n + 1;
         }
     }
-    return k;
+
+    for (int i = 0; i < n; i++)
+    {
+        int mark = abs(ar[i]) - 1;
+        if ((mark >= 0 && mark < n) && ar[mark] > 0)
+        {
+            ar[mark] *= -1;
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (ar[i - 1] > 0)
+        {
+            return i;
+        }
+    }
+    return n + 1;
 }
 };
