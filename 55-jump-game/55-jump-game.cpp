b515e4a47133487eeeccd28de8f447d24b9a8811
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-15 21:10:24 +0530

    Time: 100 ms (42.74%), Space: 48.4 MB (42.69%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-    bool canJump(vector<int> &nums, int si, int ei, int *ar, int n)
-{
-    if (si == ei)
-    {
-        return true;
-    }
-    if (ar[n] != -1)
-    {
-        return ar[n];
-    }
-
-    for (int i = 1; i <= nums[si]; i++)
-    {
-        int ans = canJump(nums, si + i, ei , ar , n - i);
-        if (ans == true)
-        {
-            ar[n] = true;
-            return ar[n];
-        }
-    }
-    ar[n] = false;
-    return ar[n];
-}
-
-bool canJump(vector<int> &nums)
+    bool canJump(vector<int> &nums)
 {
     int n = nums.size();
-    int *ar = new int[n + 1];
-    for (int i = 0; i <= n; i++)
+    int last = n - 1;
+    for (int i = n - 2; i >= 0; i--)
     {
-        ar[i] = -1;
+        if (i + nums[i] >= last)
+        {
+            last = i;
+        }
     }
-    return canJump(nums, 0, nums.size() - 1, ar, n);
+    return (last == 0);
 }
 };
