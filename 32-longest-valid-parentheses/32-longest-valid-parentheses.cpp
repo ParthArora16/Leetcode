61caea5d523fd5fd410239d180f6f6edb91f0ed0
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-15 10:49:35 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (66.86%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int longestValidParentheses(string s)
 {
-    stack<char> s1;
-    stack<int> index;
+    stack<int> s1;
     int ans = 0;
-    index.push(-1);
+    s1.push(-1);
     for (int i = 0; s[i] != '\0'; i++)
     {
         if (s[i] == '(')
         {
-            s1.push('(');
-            index.push(i);
+            s1.push(i);
         }
         else
         {
+            s1.pop();
             if (s1.empty())
             {
-                index.push(i);
+                s1.push(i);
             }
-            if (!s1.empty())
+            else
             {
-                s1.pop();
-                index.pop();
-                ans = max(ans, i - index.top());
+                ans = max(ans, i - s1.top());
             }
         }
     }
