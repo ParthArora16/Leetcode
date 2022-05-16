aaa35698a71080cf37bb1a341c96e1be5c53370b
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-16 19:34:28 +0530

    Time: 235 ms (39.08%), Space: 17 MB (40.49%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    long long int jump(vector<int> &nums , int curr, int dest,vector<int> &dp)
-    {       
-            if(curr==dest) return 0;
-            if(dp[curr]!=-1) return dp[curr];
-            //cout<<curr<<" ";
-            long long int tmp=INT_MAX;
-     
-            for(int i=1;i<=nums[curr];i++)
-            {   if(i+curr>dest) break;
-                tmp=min(tmp,1+jump(nums,curr+i,dest,dp));  
+    int jump(vector<int> &nums)
+{
+    int goal = nums.size() - 1;
+    stack<int> s1;
+    int count = 0;
+    while (goal > 0)
+    {
+        for (int i = goal - 1; i >= 0; i--)
+        {
+            if (nums[i] >= goal - i)
+            {
+                s1.push(i);
             }
-         dp[curr]=tmp;
-         return tmp;  
+        }
+        goal = s1.top();
+        count++;
+        s1.pop();
     }
-    
-    int jump(vector<int>& nums) {
-         vector<int> dp(nums.size(),-1);
-         return jump(nums,0,nums.size()-1,dp);
-    }
+    return count;
+}
 };
