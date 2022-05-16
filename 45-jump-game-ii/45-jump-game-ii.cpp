22de1990495a0c397495c631e40263e25c1d55e5
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-16 19:38:31 +0530

    Time: 98 ms (44.42%), Space: 16.5 MB (81.06%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int jump(vector<int> &nums)
+int jump(vector<int> &nums)
 {
     int goal = nums.size() - 1;
-    stack<int> s1;
+    int pointer = 0;
     int count = 0;
     while (goal > 0)
     {
+        pointer = 0;
         for (int i = goal - 1; i >= 0; i--)
         {
             if (nums[i] >= goal - i)
             {
-                s1.push(i);
+                pointer = i;
             }
         }
-        goal = s1.top();
+        goal = pointer;
         count++;
     }
     return count;
