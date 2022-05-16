f282f5cf17a30be08593e9eee3f362b734845611
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-16 13:27:31 +0530

    Time: 5 ms (23.91%), Space: 6.6 MB (52.32%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -2,35 +2,18 @@ class Solution {
     public:
         int lengthOfLastWord(string s) {
 
-            // Edge Case
             if(s.length()==1){
                 return 1;
             }
-
-
-			// Initialise pointer towards end
-			// of string
             int j = s.length()-1;
-
-			// make sure pointer points to
-			// char and ignores empty space
             while(s[j] == ' '){
                 j--;
             }
-
-
-			// counter for word
             int counter = 0;
-
-			// while pointer is greater than 0
-			// and we are not at empty char
             while(j>=0 && s[j] != ' '){
                 counter++;
-				
-				// decrement pointer
                 j--;
             }
-            
             return counter;
         }
 };
