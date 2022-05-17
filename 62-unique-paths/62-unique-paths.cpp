145730161725c72f053acfa7295a70c26b0a98f3
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-17 17:46:04 +0530

    Time: 4 ms (25.00%), Space: 6.3 MB (51.41%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n)
+{
+    int **ar = new int *[m];
+    for (int i = 0; i < m; i++)
+    {
+        ar[i] = new int[n];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        ar[m - 1][j] = 1;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        ar[i][n - 1] = 1;
+    }
+    for (int i = m - 2; i >= 0; i--)
+    {
+        for (int j = n - 2; j >= 0; j--)
+        {
+            ar[i][j] = ar[i + 1][j] + ar[i][j + 1];
+        }
+    }
+    return ar[0][0];
+}
+};
