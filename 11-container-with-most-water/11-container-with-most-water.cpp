f4621726f5ad116a7b4e155ced43ee653939af83
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-20 11:22:54 +0530

    Time: 131 ms (39.50%), Space: 59 MB (82.16%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
    int maxArea(vector<int> &height)
 {
-    int n = height.size();
     int si = 0;
-    int ei = n - 1;
-    int ans = 0;
+    int ei = height.size() - 1;
+    int fnas = 0;
+
     while (si < ei)
     {
-        int smallans = (ei - si) * min(height[si], height[ei]);
-        if (height[si] < height[ei])
+        int ans = (ei - si) * min(height[si], height[ei]);
+        if (height[ei] > height[si])
         {
+            // max height ans max distance
             si++;
         }
         else
         {
+            // height at ei will be max for others heighs greater ans smaller than height of si
+            // and width decreases therefore it will never resut to maximum;
             ei--;
         }
-        ans = max(smallans, ans);
+        fnas = max(fnas, ans);
     }
-    return ans;
+    return fnas;
 }
 };
