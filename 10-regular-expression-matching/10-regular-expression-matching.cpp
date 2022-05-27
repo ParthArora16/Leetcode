cad60a887b5fe2ba5b8bec188acfd51e0ee5257b
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-27 17:34:56 +0530

    Time: 335 ms (5.01%), Space: 13.5 MB (17.42%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,72 +1,27 @@
 class Solution {
 public:
-    bool isMatch(string s, string p, int m, int n, bool **ar)
-{
-    if (p.size() == 0)
-    {
-        return s.size() == 0;
-    }
-
-    if (ar[m][n] != false)
-    {
-        return ar[m][n];
-    }
-
-    if (p.size() > 1 && p[1] == '*')
-    {
-        // case 1
-        if (isMatch(s, p.substr(2), m, n - 2, ar))
-        {
-            ar[m][n] = true;
-            return ar[m][n];
-        }
-        if (s.length() > 0 && (s[0] == p[0] || p[0] == '.'))
-        {
-            ar[m][n] = isMatch(s.substr(1), p, m - 1, n, ar);
-            return ar[m][n];
-        }
-        ar[m][n] = false;
-        return ar[m][n];
-    }
-
-    else
-    {
-        if ((s[0] == p[0] || p[0] == '.') && s.size() > 0)
-        {
-            ar[m][n] = isMatch(s.substr(1), p.substr(1), m - 1, n - 1, ar);
-            return ar[m][n];
-        }
-        ar[m][n] = false;
-        return ar[m][n];
-    }
-    return ar[m][n];
-}
-
 bool isMatch(string s, string p)
 {
-    int m = s.length() + 1;
-    int n = p.length() + 1;
-
-    bool **ar = new bool *[m];
-
-    for (int i = 0; i < m; i++)
+    if (p.length() == 0)
     {
-        ar[i] = new bool[n];
-        for (int j = 0; j < n; j++)
+        return s.length() == 0;
+    }
+    if (p.length() > 1 && p[1] == '*')
+    {
+        if (isMatch(s, p.substr(2)))
         {
-            ar[i][j] = false;
+            return true;
         }
+        if (s[0] == p[0] || p[0] == '.' && s.length() > 0)
+        {
+            return isMatch(s.substr(1), p);
+        }
+        return false;
     }
-
-    bool ans = isMatch(s, p, m - 1, n - 1, ar);
-
-    for (int i = 0; i < m; i++)
+    if (s[0] == p[0] || p[0] == '.' && s.length() > 0)
     {
-        delete[] ar[i];
+        return isMatch(s.substr(1), p.substr(1));
     }
-
-    delete[] ar;
-
-    return ans;
+    return false;
 }
 };
