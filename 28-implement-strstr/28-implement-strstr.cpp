37a0e9357855db32598422f0bbcca393dcb27e2d
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-05-30 13:08:03 +0530

    Time: 3 ms (42.24%), Space: 6.4 MB (25.00%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-int strStr(string s1, string s2)
+    int strStr(string s1, string s2)
 {
-    if (s1.length() < s2.length())
-    {
-        return -1;
-    }
-    if (s1.substr(0, s2.size()) == s2)
+    if (s2.size() == 0)
     {
         return 0;
     }
-    int ans = strStr(s1.substr(1), s2);
-    if (ans != -1)
+    for (int i = 0; i < s1.size(); i++)
     {
-        return ans + 1;
+        if ((s1[i] == s2[0]) && (s1.substr(i, s2.size()) == s2))
+        {
+            return i;
+        }
     }
     return -1;
 }
