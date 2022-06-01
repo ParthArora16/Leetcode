4f5a08671077950f1c3801e305bf25829f44d0b9
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-06-01 10:35:28 +0530

    Time: 42 ms (79.40%), Space: 19.1 MB (67.83%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>> &v)
+{
+    vector<vector<int>> res;
+    if (v.size() == 0)
+    {
+        return res;
+    }
+    sort(v.begin(), v.end());
+    res.push_back(v[0]);
+    int index = 0;
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (res[index][1] >= v[i][0])
+        {
+            res[index][1] = max(res[index][1], v[i][1]);
+        }
+        else
+        {
+            index++;
+            res.push_back(v[i]);
+        }
+    }
+    return res;
+}
+};
