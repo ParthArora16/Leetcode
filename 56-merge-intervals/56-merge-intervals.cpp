b2a7409305bacd01f87612e8ae38c4b723235347
ParthArora16 <96593555+ParthArora16@users.noreply.github.com>
2022-06-01 10:41:23 +0530

    Time: 77 ms (19.96%), Space: 19 MB (67.83%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -10,14 +10,20 @@ vector<vector<int>> merge(vector<vector<int>> &v)
     {
         if (res[index][1] >= v[i][0])
         {
+            // overlaps 
+            // just update my resultant
             res[index][1] = max(res[index][1], v[i][1]);
         }
         else
         {
-            index++;
+            // not overlaps
+            // push that interval in resultant
             res.push_back(v[i]);
+            // index is now increased
+            index++;
         }
     }
     return res;
 }
+
 };
